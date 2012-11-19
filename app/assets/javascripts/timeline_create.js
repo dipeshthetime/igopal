c0622e6020132580b1c7b75904c757f7a96dc688
Debbie Hoffpauir <it_yogini@gmail.com>
2012-11-18 19:23:34 -0600

    Minor edits for display linking;
    exit

--- app/assets/javascripts/timeline_create.js
+++ app/assets/javascripts/timeline_create.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
+
     createStoryJS({
         type:       'timeline',
         width:      "100%",
         height:     '600',                                                        
-  		source: 	"portfolios/1.json",
+  		source: 	"portfolios/5.json",
         embed_id:   "my-timeline"
     });
 });
