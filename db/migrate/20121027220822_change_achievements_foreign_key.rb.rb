d271bfb9d6da7534f87d521c65088cdf1dad8d5c
ginuk07 <ginuk07@gmail.com>
2012-11-25 23:02:01 -0600

    Prod DB

---
+++ db/migrate/20121027220822_change_achievements_foreign_key.rb.rb
@@ -0,0 +1,18 @@
+class ChangeAchievementsForeignKey < ActiveRecord::Migration
+  def up
+
+     change_table :achievements do |t|
+         t.references :user
+         remove_column :achievements, :portfolio_id
+     end
+  end
+
+  def down
+      change_table :achievements do |t|
+       remove_column :achievements, :user_id
+       t.references :portfolio
+       remove_column :achievements, :portfolio_id_id
+       remove_column :achievements, :achievements_id
+   end
+  end
+end
