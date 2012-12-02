02626491bed40b2444538cac1bd1761917101d70
ginuk07 <ginuk07@gmail.com>
2012-12-01 20:37:21 -0600

    Paperclip Upload Again...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Igopal::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.paperclip_defaults = {
+config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
