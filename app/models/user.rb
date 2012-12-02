f50bbf9458e345b407c95a2c1e946bb4ac975de0
ginuk07 <ginuk07@gmail.com>
2012-12-01 19:22:33 -0600

    AWS s3 Test

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :id, :role_ids
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :id, :role_ids, :avatar
   has_one :portfolio, :dependent => :destroy, :inverse_of => :user
   has_many :achievements, :through => :portfolio , :include => :achievements
   validates_presence_of :email, :first_name, :last_name, :login_name
