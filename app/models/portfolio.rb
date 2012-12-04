de84524396f5aca8dd77b48ba66abf37c1b6d4f1
ginuk07 <ginuk07@gmail.com>
2012-12-03 19:17:42 -0600

    Dropbox Test 2

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,8 +6,13 @@ class Portfolio < ActiveRecord::Base
   	attr_accessible :id, :achievements, :user_id, :title, :variety, :start_date, :intro, 
   		:image_file_name, :image_content_type , :image,  :startDate,
   		:image_file_size, :image_updated_at
-  	has_attached_file :image, :dependent => :destroy
-
+  	
+  has_attached_file :image,
+    :storage => :dropbox,
+    :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
+    :dropbox_options => {
+      :path => proc { |style| "#{style}/#{id}_#{image.original_filename}"}
+    }
 def init
   self.variety ||='default'
 end
